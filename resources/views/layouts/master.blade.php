1bf9022be77f2a9372f785ae44f82fd05e8394fb
T-Waddell <tara.waddell2@gmail.com>
2018-10-31 23:12:50 -0500

    Created master layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang='en'>
+<head>
+    <title>@yield('title', config('app.name'))</title>
+    <meta charset='utf-8'>
+
+    {{-- CSS global to every page can be loaded here --}}
+    <link href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" rel="stylesheet" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href='/css/styles.css' rel='stylesheet'>
+</head>
+<body>
+
+<header>
+    <a href='/'><img src='/images/foobooks-logo@2x.png' id='logo' alt='Foobooks Logo'></a>
+</header>
+
+<section id='main'>
+    @yield('content')
+</section>
+
+<footer>
+    <a href='http://github.com/T-Waddell/p3'><i class='fab fa-github'></i> View on Github</a> |
+                                                                                                    &copy; {{ date('Y') }}
+</footer>
+
+</body>
+</html>
--- resources/views/practice.blade.php
+++ resources/views/practice.blade.php
@@ -1,18 +1,9 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
 
-    <title>Foobooks</title>
 
-</head>
-<body>
-<h1>Practice</h1>
-@foreach($methods as $method)
-    <a href='{{ str_replace('practice', '/practice/', $method) }}'> {{ $method }}</a><br>
-@endforeach
-
-</body>
-</html>
+@section('content')
+    <h1>Practice</h1>
+    @foreach($methods as $method)
+        <a href='{{ str_replace('practice', '/practice/', $method) }}'> {{ $method }}</a><br>
+    @endforeach
+@endsection

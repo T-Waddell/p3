61bea4a8bab518b8d87d25997a66f5de1f66bebe
T-Waddell <tara.waddell2@gmail.com>
2018-10-31 22:02:12 -0500

    Got practice.blade.php view to work.

--- resources/views/practice.blade.php
+++ resources/views/practice.blade.php
@@ -1,9 +1,18 @@
-@extends('layouts.master')
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <title>Foobooks</title>
 
-@section('content')
-    <h1>Practice</h1>
-    @foreach($methods as $method)
-        <a href='{{ str_replace('practice', '/practice/', $method) }}'> {{ $method }}</a><br>
-    @endforeach
-@endsection
+</head>
+<body>
+<h1>Practice</h1>
+@foreach($methods as $method)
+    <a href='{{ str_replace('practice', '/practice/', $method) }}'> {{ $method }}</a><br>
+@endforeach
+
+</body>
+</html>
